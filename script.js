77a49bdc7ac3f2f422dbe4ec65c1b3421687dc21
aymane <elmainiaymane03@gmail.com>
2023-06-25 01:04:09 +0100

    last touches

--- script.js
+++ script.js
@@ -3,45 +3,43 @@ const display_input = document.querySelector('.display .input');
 const display_output = document.querySelector('.display .output');
 
 let input = "";
-for (let key of keys){
+for (let key of keys) {
   const value = key.dataset.key;
 
-  key.addEventListener('click', () =>{
-    if (value == 'clear'){
+  key.addEventListener('click', () => {
+    if (value == 'clear') {
       input = "";
       display_input.innerHTML = "";
       display_output.innerHTML = "";
-    }else if (value == "backspace"){
+    } else if (value == "backspace") {
       input = input.slice(0, -1);
       display_input.innerHTML = input;
-    }else if (value == "="){
+    } else if (value == "=") {
       let result = eval(input);
-
       display_output.innerHTML = result;
-    }else if (valuee == "brackets"){
-
+    } else if (value == "brackets") {
       if (
-      input.indexOf("(") == -1 ||
-      input.indexOf("(") != -1 &&
-      input.indexOf(")") != -1 &&
-      input .lastIndexOf(")")
-      ){
-        input +="(";
-      }
-      else if (
-        input.indexOf("(") != -1 && 
-        input.indexOf(")") == -1 ||
+        input.indexOf("(") == -1 ||
+        (input.indexOf("(") != -1 &&
+          input.indexOf(")") != -1 &&
+          input.lastIndexOf(")"))
+      ) {
+        input += "(";
+      } else if (
         input.indexOf("(") != -1 &&
-        input.indexOf(")") != -1 &&
-        input.lastIndexOf("(") < input.lastIndexOf(")")
-        ){
-        input -= ")";
+        input.indexOf(")") == -1 ||
+        (input.indexOf("(") != -1 &&
+          input.indexOf(")") != -1 &&
+          input.lastIndexOf("(") < input.lastIndexOf(")")
+        )
+      ) {
+        input += ")";
       }
 
       display_input.innerHTML = input;
-    }else{
+    } else {
       input += value;
       display_input.innerHTML = input;
     }
-  })
+  });
 }
